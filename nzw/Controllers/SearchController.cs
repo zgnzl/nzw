fa27fb9e1ce7fdcbec856473e294c9df0ce3b6a9
niezl <nzlwan@126.com>
2018-03-01 09:50:32 +0800

    检索

--- nzw/Controllers/SearchController.cs
+++ nzw/Controllers/SearchController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,6 +18,9 @@ namespace nzw.Controllers
         public ActionResult Pub(string url)
         {
             ViewBag.url = url;
+            WebClient client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            ViewBag.content= client.DownloadString(url);
             return View();
         }
     }
